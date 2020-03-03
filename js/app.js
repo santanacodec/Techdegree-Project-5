3f47fa651ccaa59e4f1db40a76e2de3aaa380e36
Edgar Santana <santanaprogrammer@gmail.com>
2020-03-03 11:08:16 -0500

    updated lightbox plugin
    changed to magnific popup and got it to work

--- js/app.js
+++ js/app.js
@@ -1,23 +1,3 @@
 $(document).ready(function () {
-
-	/* This is basic - uses default settings */
-
-	// $("a#single_image").lightbox();
-
-	/* Using custom settings */
-
-	$("a#inline").lightbox({
-		'hideOnContentClick': true
-	});
-
-	/* Apply fancybox to multiple items */
-
-	$("a.group").lightbox({
-		'transitionIn': 'elastic',
-		'transitionOut': 'elastic',
-		'speedIn': 600,
-		'speedOut': 200,
-		'overlayShow': false
-	});
-
+    $('.image-link').magnificPopup({ type: 'image' });
 });

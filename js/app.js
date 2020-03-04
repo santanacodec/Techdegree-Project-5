c0bd4c891b0e2eead9cc384320e2bd9bdde4af96
Edgar Santana <santanaprogrammer@gmail.com>
2020-03-03 19:40:22 -0500

    finished lightbox
    finished up the lightbox using magnific popup

--- js/app.js
+++ js/app.js
@@ -1,3 +1,15 @@
 $(document).ready(function () {
-    $('.image-link').magnificPopup({ type: 'image' });
+    $('.container').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        gallery: {
+            enabled: true 
+        },
+        image: {
+            titleSrc: function (item) {
+                return item.el.find('img').attr('title');
+            }
+        },
+        showCloseBtn: false
+    });
 });
